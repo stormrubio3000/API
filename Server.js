8bd4369a9d43449d7e0ba125e49df84a89470ffe
stormrubio3000 <32712771+stormrubio3000@users.noreply.github.com>
2018-04-27 21:23:27 -0500

    Server changes
    Took out the HTML formatting

--- Server.js
+++ Server.js
@@ -4,9 +4,8 @@ var url = require('url');
 http.createServer(function (req, res) {
   const { headers, method, url } = req;
   res.writeHead(200, {'Content-Type': 'text/html'});
-  console.log(url);               
+  console.log(url);
   var call = url.substr(1);
-  console.log(call);
   
   
   const WolframAlphaAPI = require('wolfram-alpha-api');
@@ -14,7 +13,7 @@ http.createServer(function (req, res) {
 
   API();
   function API(){
-    if(call ==="/"){
+    if(call ===""){
       res.end('<html><body><h1>There was an error please refresh.</h1></body></html>');
     }
     else{
@@ -22,9 +21,9 @@ http.createServer(function (req, res) {
       const pods = queryresult.pods;
       const output = pods.map((pod) => {
       const subpodContent = pod.subpods.map(subpod =>
-        `  <img src="${subpod.img.src}" alt="${subpod.img.alt}">`
+        `alt="${subpod.img.alt}">`
       ).join('\n');
-      return `<h2>${pod.title}</h2>\n${subpodContent}`;
+      return `${pod.title}\n${subpodContent}`;
       }).join('\n');
     res.end(output);
   }).catch(console.error);
